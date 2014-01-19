487b5b209b5baf573564bd376025f1f4b369ae83
Ariane van der Steldt <ariane@stack.nl>
2014-01-19 20:13:01 +0000

    Implement c++ initializer_list.

---
+++ abi/include/initializer_list-inl.h
@@ -0,0 +1,55 @@
+namespace std {
+
+
+template<typename E>
+constexpr initializer_list<E>::initializer_list(
+    const initializer_list<E>* p, initializer_list<E>::size_type s) {
+: begin_(p),
+  sz_(s)
+{}
+
+template<typename E>
+inline initializer_list<E>::const_iterator initializer_list<E>::begin()
+    const noexcept {
+  return begin_;
+}
+
+template<typename E>
+inline initializer_list<E>::const_iterator initializer_list<E>::end()
+    const noexcept {
+  return begin_ + sz_;
+}
+
+template<typename E>
+inline initializer_list<E>::iterator initializer_list<E>::begin() noexcept {
+  return begin_;
+}
+
+template<typename E>
+inline initializer_list<E>::iterator initializer_list<E>::end() noexcept {
+  return begin_ + sz_;
+}
+
+
+template<typename E>
+inline initializer_list<E>::iterator begin(initializer_list<E>& l) noexcept {
+  return l.begin();
+}
+template<typename E>
+inline initializer_list<E>::iterator end(initializer_list<E>& l) noexcept {
+  return l.end();
+}
+
+template<typename E>
+inline initializer_list<E>::const_iterator begin(const initializer_list<E>& l)
+    noexcept {
+  return l.begin();
+}
+template<typename E>
+inline initializer_list<E>::const_iterator end(const initializer_list<E>& l)
+    noexcept {
+  return l.end();
+}
+
+
+} /* namespace std */
