feb9f901fb7a780d8863212775c5b67bc1ee37ec
Ariane van der Steldt <ariane@stack.nl>
2014-01-21 23:03:17 +0000

    Fix initializer_list.

--- abi/include/initializer_list-inl.h
+++ abi/include/initializer_list-inl.h
@@ -3,51 +3,56 @@ namespace std {
 
 template<typename E>
 constexpr initializer_list<E>::initializer_list(
-    const initializer_list<E>* p, initializer_list<E>::size_type s) {
+    const initializer_list<E>::value_type* p,
+    initializer_list<E>::size_type s)
 : begin_(p),
   sz_(s)
 {}
 
 template<typename E>
-inline initializer_list<E>::const_iterator initializer_list<E>::begin()
+inline typename initializer_list<E>::const_iterator initializer_list<E>::begin()
     const noexcept {
   return begin_;
 }
 
 template<typename E>
-inline initializer_list<E>::const_iterator initializer_list<E>::end()
+inline typename initializer_list<E>::const_iterator initializer_list<E>::end()
     const noexcept {
   return begin_ + sz_;
 }
 
 template<typename E>
-inline initializer_list<E>::iterator initializer_list<E>::begin() noexcept {
+inline typename initializer_list<E>::iterator initializer_list<E>::begin()
+    noexcept {
   return begin_;
 }
 
 template<typename E>
-inline initializer_list<E>::iterator initializer_list<E>::end() noexcept {
+inline typename initializer_list<E>::iterator initializer_list<E>::end()
+    noexcept {
   return begin_ + sz_;
 }
 
 
 template<typename E>
-inline initializer_list<E>::iterator begin(initializer_list<E>& l) noexcept {
+inline typename initializer_list<E>::iterator begin(initializer_list<E>& l)
+    noexcept {
   return l.begin();
 }
 template<typename E>
-inline initializer_list<E>::iterator end(initializer_list<E>& l) noexcept {
+inline typename initializer_list<E>::iterator end(initializer_list<E>& l)
+    noexcept {
   return l.end();
 }
 
 template<typename E>
-inline initializer_list<E>::const_iterator begin(const initializer_list<E>& l)
-    noexcept {
+inline typename initializer_list<E>::const_iterator begin(
+    const initializer_list<E>& l) noexcept {
   return l.begin();
 }
 template<typename E>
-inline initializer_list<E>::const_iterator end(const initializer_list<E>& l)
-    noexcept {
+inline typename initializer_list<E>::const_iterator end(
+    const initializer_list<E>& l) noexcept {
   return l.end();
 }
 
