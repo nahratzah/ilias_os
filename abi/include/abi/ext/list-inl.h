8a34d8e3aa79b7575c4974177bc98f6ffc317f0e
Ariane van der Steldt <ariane@stack.nl>
2014-01-25 01:08:57 +0000

    Implement an intrusive linked list.

---
+++ abi/include/abi/ext/list-inl.h
@@ -0,0 +1,328 @@
+namespace __cxxabiv1 {
+namespace ext {
+
+
+template<typename Tag> constexpr list_elem<Tag>::list_elem() {}
+
+template<typename Tag> constexpr list_elem<Tag>::list_elem(list_elem* init)
+: succ_{ init },
+  pred_{ init }
+{}
+
+template<typename Tag> constexpr list_elem(const list_elem&) {}
+
+template<typename Tag> constexpr list_elem& operator=(const list_elem&) {
+  return *this;
+}
+
+
+template<typename T, typename Tag>
+list<T, Tag>::list(list&& o) noexcept {
+  head_.succ_ = o.head_.succ_;
+  head_.pred_ = o.head_.pred_;
+  o.head_.succ_ = &o.head_;
+  o.head_.pred_ = &o.head_;
+  head_.succ_.pred_ = &head_;
+  head_.pred_.succ_ = &head_;
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::operator=(list&& o) noexcept -> list& {
+  swap(o);
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::swap(list& o) noexcept -> void {
+  using std::swap;
+
+  swap(head_.succ_, o.head_.succ_);
+  swap(head_.pred_, o.head_.pred_);
+  head_.succ_.pred_ = head_.pred_.succ_ = &head_;
+  o.head_.succ_.pred_ = o.head_.pred_.succ_ = &o.head_;
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::empty() const noexcept -> bool {
+  return size() == nullptr;
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::is_linked(const_pointer p) const noexcept -> bool {
+  return (p && p->succ_ != nullptr);
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::link_front(pointer p) noexcept -> bool {
+  return link_after_(p, &head_);
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::link_back(pointer p) noexcept -> bool {
+  return link_before_(p, &head_);
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::unlink(pointer e) noexcept -> bool {
+  if (!is_linked(e)) return false;
+  const pointer s = e->succ_;
+  const pointer p = e->pred_;
+
+  s->pred_ = p;
+  p->succ_ = s;
+  e->succ_ = nullptr;
+  e->pred_ = nullptr;
+  return true;
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::link_before(pointer e, const_iterator pos) noexcept {
+  const list_elem<Tag>* p = &*pos;
+  return link_before_(e, const_cast<list_elem<Tag>*>(p));
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::link_after(pointer e, const_iterator pos) noexcept {
+  const list_elem<Tag>* p = &*pos;
+  return link_after_(e, const_cast<list_elem<Tag>*>(p));
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::unlink(const_iterator pos) noexcept {
+  const list_elem<Tag>* p = &*pos;
+  return unlink(const_cast<list_elem<Tag>*>(p));
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::begin() noexcept -> iterator {
+  return iterator{ succ(&head) };
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::end() noexcept -> iterator {
+  return iterator{ &head };
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::begin() const noexcept -> const_iterator {
+  return const_iterator{ succ(&head) };
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::end() const noexcept -> const_iterator {
+  return const_iterator{ &head };
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::rbegin() noexcept -> reverse_iterator {
+  return reverse_iterator{ pred(&head) };
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::rend() noexcept -> reverse_iterator {
+  return reverse_iterator{ &head };
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::rbegin() const noexcept -> const_reverse_iterator {
+  return const_reverse_iterator{ pred(&head) };
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::rend() const noexcept -> const_reverse_iterator {
+  return const_reverse_iterator{ &head };
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::cbegin() const noexcept -> const_iterator {
+  return begin();
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::cend() const noexcept -> const_iterator {
+  return end();
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::crbegin() const noexcept -> const_reverse_iterator {
+  return rbegin();
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::crend() const noexcept -> const_reverse_iterator {
+  return rend();
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::link_after_(pointer e, pointer p) noexcept -> bool {
+  if (!e || is_linked(e)) return false;
+  const pointer s = p->succ_;
+
+  p->succ_ = e;
+  s->pred_ = e;
+  e->pred_ = p;
+  e->succ_ = s;
+  return true;
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::link_before_(pointer e, pointer s) noexcept -> bool {
+  return link_after_(e, s->pred_);
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::pred(list_elem<Tag>* e) noexcept -> list_elem<Tag>* {
+  return (e ? e->pred_ : nullptr);
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::pred(const list_elem<Tag>* e) noexcept ->
+    const list_elem<Tag>* {
+  return (e ? e->pred_ : nullptr);
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::succ(list_elem<Tag>* e) noexcept -> list_elem<Tag>* {
+  return (e ? e->succ_ : nullptr);
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::succ(const list_elem<Tag>* e) noexcept ->
+    const list_elem<Tag>* {
+  return (e ? e->succ_ : nullptr);
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::down_cast(list_elem<Tag>* e) noexcept -> T* {
+  return static_cast<T*>(e);
+}
+
+template<typename T, typename Tag>
+auto list<T, Tag>::down_cast(const list_elem<Tag>* e) noexcept -> const T* {
+  return static_cast<const T*>(e);
+}
+
+
+template<typename T, typename Tag>
+template<>
+auto list<T, Tag>::iterator_order<true>::pred(elem_t* e) noexcept -> elem_t* {
+  return list<T, Tag>::pred(e);
+}
+template<typename T, typename Tag>
+template<>
+auto list<T, Tag>::iterator_order<true>::pred(const_elem_t* e) noexcept ->
+    const_elem_t* {
+  return list<T, Tag>::pred(e);
+}
+template<typename T, typename Tag>
+template<>
+auto list<T, Tag>::iterator_order<true>::succ(elem_t* e) noexcept -> elem_t* {
+  return list<T, Tag>::succ(e);
+}
+template<typename T, typename Tag>
+template<>
+auto list<T, Tag>::iterator_order<true>::succ(const_elem_t* e) noexcept ->
+    const_elem_t* {
+  return list<T, Tag>::succ(e);
+}
+
+
+template<typename T, typename Tag>
+template<>
+auto list<T, Tag>::iterator_order<false>::pred(elem_t* e) noexcept -> elem_t* {
+  return list<T, Tag>::succ(e);
+}
+template<typename T, typename Tag>
+template<>
+auto list<T, Tag>::iterator_order<false>::pred(const_elem_t* e) noexcept ->
+    const_elem_t* {
+  return list<T, Tag>::succ(e);
+}
+template<typename T, typename Tag>
+template<>
+auto list<T, Tag>::iterator_order<false>::succ(elem_t* e) noexcept -> elem_t* {
+  return list<T, Tag>::pred(e);
+}
+template<typename T, typename Tag>
+template<>
+auto list<T, Tag>::iterator_order<false>::succ(const_elem_t* e) noexcept ->
+    const_elem_t* {
+  return list<T, Tag>::pred(e);
+}
+
+
+template<typename T, typename Tag>
+template<typename IT, typename IE, bool Order>
+constexpr list<T, Tag>::iterator_tmpl<IT, IE, Order>::iterator_tmpl(IE* elem)
+: elem_(elem)
+{}
+
+template<typename T, typename Tag>
+template<typename IT, typename IE, bool Order>
+auto list<T, Tag>::iterator_tmpl<IT, IE, Order>::operator*() const noexcept ->
+    reference {
+  return *down_cast(elem_);
+}
+
+template<typename T, typename Tag>
+template<typename IT, typename IE, bool Order>
+auto list<T, Tag>::iterator_tmpl<IT, IE, Order>::operator->() const noexcept ->
+    reference {
+  return down_cast(elem_);
+}
+
+template<typename T, typename Tag>
+template<typename IT, typename IE, bool Order>
+auto list<T, Tag>::iterator_tmpl<IT, IE, Order>::operator++() noexcept ->
+    iterator_tmpl& {
+  elem_ = this->succ(elem_);
+  return *this;
+}
+
+template<typename T, typename Tag>
+template<typename IT, typename IE, bool Order>
+auto list<T, Tag>::iterator_tmpl<IT, IE, Order>::operator--() noexcept ->
+    iterator_tmpl& {
+  elem_ = this->pred(elem_);
+  return *this;
+}
+
+template<typename T, typename Tag>
+template<typename IT, typename IE, bool Order>
+auto list<T, Tag>::iterator_tmpl<IT, IE, Order>::operator++(int) noexcept ->
+    iterator_tmpl {
+  iterator_tmpl rv = *this;
+  ++(*this);
+  return rv;
+}
+
+template<typename T, typename Tag>
+template<typename IT, typename IE, bool Order>
+auto list<T, Tag>::iterator_tmpl<IT, IE, Order>::operator--(int) noexcept ->
+    iterator_tmpl {
+  iterator_tmpl rv = *this;
+  --(*this);
+  return rv;
+}
+
+template<typename T, typename Tag>
+template<typename IT, typename IE, bool Order>
+template<typename O_IT, typename O_IE, bool O_Order>
+auto list<T, Tag>::iterator_tmpl<IT, IE, Order>::operator=(
+    const iterator_tmpl<O_IT, O_IE, O_Order>& o) noexcept -> iterator_tmpl& {
+  elem_ = &*o;
+  return *this;
+}
+
+
+template<typename T, typename Tag>
+auto swap(list<T, Tag>& a, list<T, Tag>& b) noexcept -> void {
+  return a.swap(b);
+}
+
+template<typename Tag, typename T>
+auto unlink(T* e) noexcept -> bool {
+  return list<T, Tag>::unlink(e);
+}
+
+
+}} /* namespace __cxxabiv1::ext */
---
+++ abi/include/abi/ext/list.h
@@ -0,0 +1,185 @@
+#ifndef _ABI_EXT_LIST_H_
+#define _ABI_EXT_LIST_H_
+
+#include <abi/abi.h>
+#include <iterator>
+
+namespace __cxxabiv1 {
+namespace ext {
+
+
+template<typename T, typename Tag> class list;
+
+
+template<typename Tag> class list_elem {
+  friend template<typename T> list<T, Tag>;
+
+ public:
+  constexpr list_elem();
+  constexpr list_elem(const list_elem&);
+  constexpr list_elem& operator=(const list_elem&);
+
+ protected:
+  ~list_elem() = default;
+
+ private:
+  constexpr list_elem(list_elem*);
+
+  list_elem* succ_ = nullptr;
+  list_elem* pred_ = nullptr;
+};
+
+
+template<typename T, typename Tag> class list {
+ private:
+  template<bool Order> class iterator_order;
+  template<typename IT, typename IE, bool Order> class iterator_tmpl;
+
+ public:
+  using size_type = size_t;
+  using difference_type = ptrdiff_t;
+  using value_type = T;
+  using reference = value_type&;
+  using const_reference = const value_type&;
+  using pointer = value_type*;
+  using const_pointer = const value_type*;
+
+  using iterator = iterator_tmpl<value_type, list_elem<Tag>, true>;
+  using const_iterator = iterator_tmpl<const value_type,
+                                       const list_elem<Tag>, true>;
+  using reverse_iterator = iterator_tmpl<value_type, list_elem<Tag>, false>;
+  using const_reverse_iterator = iterator_tmpl<const value_type,
+                                               const list_elem<Tag>, false>;
+
+  list() = default;
+  list(const list&) = delete;
+  list& operator=(const list&) = delete;
+  list& operator=(list&&) noexcept;
+  ~list() = default;
+
+  void swap(list&) noexcept;
+  bool empty() const noexcept;
+  size_type size() const noexcept;
+
+  static bool is_linked(const_pointer) const noexcept;
+  bool link_front(pointer) noexcept;
+  bool link_back(pointer) noexcept;
+  static bool unlink(pointer) noexcept;
+  bool link_before(pointer, const_iterator) noexcept;
+  bool link_after(pointer, const_iterator) noexcept;
+  static bool unlink(const_iterator) noexcept;  // Found via ADL.
+
+  iterator begin() noexcept;
+  iterator end() noexcept;
+  const_iterator begin() const noexcept;
+  const_iterator end() const noexcept;
+  reverse_iterator rbegin() noexcept;
+  reverse_iterator rend() noexcept;
+  const_reverse_iterator rbegin() const noexcept;
+  const_reverse_iterator rend() const noexcept;
+  const_iterator cbegin() const noexcept;
+  const_iterator cend() const noexcept;
+  const_reverse_iterator crbegin() const noexcept;
+  const_reverse_iterator crend() const noexcept;
+
+ private:
+  static bool link_after_(pointer, pointer) noexcept;
+  static bool link_before_(pointer, pointer) noexcept;
+
+  static list_elem<Tag>* pred(list_elem<Tag>*) noexcept;
+  static const list_elem<Tag>* pred(const list_elem<Tag>*) noexcept;
+  static list_elem<Tag>* succ(list_elem<Tag>*) noexcept;
+  static const list_elem<Tag>* succ(const list_elem<Tag>*) noexcept;
+  static T* down_cast(list_elem<Tag>*) noexcept;
+  static const T* down_cast(const list_elem<Tag>*) noexcept;
+
+  list_elem<Tag> head_{ &head_ };
+};
+
+
+template<typename T, typename Tag>
+template<>
+class list<T, Tag>::iterator_order<true> {
+ public:
+  using iterator_category = bidirectional_iterator_tag;
+
+ private:
+  using elem_t = list_elem<Tag>;
+  using const_elem_t = const list_elem<Tag>;
+
+ protected:
+  ~iterator_order() = default;
+
+  static elem_t* pred(elem_t*) noexcept;
+  static const_elem_t* pred(const_elem_t*) noexcept;
+  static elem_t* succ(elem_t*) noexcept;
+  static const_elem_t* succ(const_elem_t*) noexcept;
+};
+
+template<typename T, typename Tag>
+template<>
+class list<T, Tag>::iterator_order<false> {
+ public:
+  using iterator_category = bidirectional_iterator_tag;
+
+ private:
+  using elem_t = list_elem<Tag>;
+  using const_elem_t = const list_elem<Tag>;
+
+ protected:
+  ~iterator_order() = default;
+
+  static elem_t* pred(elem_t*) noexcept;
+  static const_elem_t* pred(const_elem_t*) noexcept;
+  static elem_t* succ(elem_t*) noexcept;
+  static const_elem_t* succ(const_elem_t*) noexcept;
+};
+
+
+template<typename T, typename Tag>
+template<typename IT, typename IE, bool Order>
+class list<T, Tag>::iterator_tmpl
+: public typename list<T, Tag>::template iterator_order<Order> {
+  friend list<T, Tag>;
+
+ public:
+  using value_type = IT;
+  using reference = value_type&;
+  using pointer = value_type*;
+  using size_type = list<T, Tag>::size_type;
+  using difference_type = list<T, Tag>::difference_type;
+
+  iterator_tmpl() = default;
+  iterator_tmpl(const iterator_tmpl&) = default;
+  iterator_tmpl& operator=(const iterator_tmpl&) = default;
+
+  template<typename O_IT, typename O_IE, bool O_Order>
+  iterator_tmpl& operator=(const iterator_tmpl<O_IT, O_IE, O_Order>& o)
+      noexcept;
+
+  reference operator*() const noexcept;
+  pointer operator->() const noexcept;
+  iterator_tmpl& operator++() noexcept;
+  iterator_tmpl& operator--() noexcept;
+  iterator_tmpl operator++(int) noexcept;
+  iterator_tmpl operator--(int) noexcept;
+
+ private:
+  explicit constexpr iterator_tmpl(IE* elem);
+
+  IE* elem_ = nullptr;
+};
+
+
+template<typename T, typename Tag>
+void swap(list<T, Tag>&, list<T, Tag>&) noexcept;
+
+template<typename Tag, typename T>
+bool unlink(T*) noexcept;
+
+
+}} /* namespace __cxxabiv1::ext */
+
+#include <abi/ext/list-inl.h>
+
+#endif /* _ABI_EXT_LIST_H_ */
