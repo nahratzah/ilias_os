a45c0dad491449830271e542c18b9e6059eddd00
Ariane van der Steldt <ariane@stack.nl>
2014-02-03 09:02:53 +0000

    Do actually test the local implementation, instead of the reference impl.

--- abi/test/cstring/memrchr.cc
+++ abi/test/cstring/memrchr.cc
@@ -3,7 +3,7 @@
 
 int main() {
   const char* abba = "abba";
-  if (memrchr(abba, 'a', strlen(abba)) != &abba[3]) return 1;
-  if (memrchr(abba, 'b', strlen(abba)) != &abba[2]) return 2;
-  if (memrchr(abba, 'b', strlen(abba) - 2) != &abba[1]) return 3;
+  if (::test_std::memrchr(abba, 'a', strlen(abba)) != &abba[3]) return 1;
+  if (::test_std::memrchr(abba, 'b', strlen(abba)) != &abba[2]) return 2;
+  if (::test_std::memrchr(abba, 'b', strlen(abba) - 2) != &abba[1]) return 3;
 }
