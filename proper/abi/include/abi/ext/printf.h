758396a01919bed51edb7bd21660e90f08379604
Ariane van der Steldt <ariane@stack.nl>
2014-02-24 02:20:14 +0000

    Change vxprintf to template.
    It started out as one, but changed to being just char* based.
    Now that basic_string_ref is implemented, I can use proper templates.
    Yay, string agnostic printf implementation.  \o/

--- proper/abi/include/abi/ext/printf.h
+++ proper/abi/include/abi/ext/printf.h
@@ -2,8 +2,12 @@
 #define _ABI_EXT_PRINTF_H_
 
 #include <abi/abi.h>
-#include <cstddef_misc/valist.h>
-#include <abi/ext/string_piece.h>
+#include <abi/panic.h>
+#include <cstdarg>
+#include <cerrno>
+#include <cstdarg>
+#include <limits>
+#include <string>
 
 namespace __cxxabiv1 {
 namespace ext {
@@ -12,23 +16,59 @@ namespace ext {
 #define NL_ARGMAX	128	// XXX find correct header file
 
 
+template<typename Char, typename Traits = std::char_traits<Char>>
 class printf_renderer {
  public:
+  using string_type = std::basic_string_ref<Char, Traits>;
+  using size_type = typename string_type::size_type;
+
   virtual ~printf_renderer() noexcept;
-  int append(c_string_piece) noexcept;
-  uintmax_t length() const noexcept { return len_; }
-  uintmax_t size() const noexcept { return len_; }
-  virtual char get_thousand_sep() const noexcept;
+  int append(string_type) noexcept;
+  template<typename C, typename T> int append(std::basic_string_ref<C, T>)
+      noexcept;
+  size_type length() const noexcept { return len_; }
+  size_type size() const noexcept { return len_; }
+  virtual Char get_thousand_sep() const noexcept;
+  virtual Char get_pos_sign() const noexcept;  // '+'
+  virtual Char get_neg_sign() const noexcept;  // '-'
 
  private:
-  virtual int do_append(c_string_piece) noexcept = 0;
+  virtual int do_append(string_type) noexcept = 0;
 
-  uintmax_t len_ = 0;
+  size_type len_ = 0;
 };
 
-int vxprintf(printf_renderer& r, c_string_piece fmt, va_list ap) noexcept;
+template<typename Char, typename Traits = std::char_traits<Char>>
+int vxprintf(printf_renderer<Char, Traits>&,
+             typename printf_renderer<Char, Traits>::string_type,
+             va_list) noexcept;
+
+template<typename Char, typename Traits = std::char_traits<Char>>
+int vxprintf(printf_renderer<Char, Traits>&,
+             typename printf_renderer<Char, Traits>::string_type,
+             ...) noexcept;
+
+extern template class printf_renderer<char>;
+extern template class printf_renderer<wchar_t>;
+extern template class printf_renderer<char16_t>;
+extern template class printf_renderer<char32_t>;
+
+extern template int vxprintf(printf_renderer<char>&,
+                             std::basic_string_ref<char>,
+                             va_list) noexcept;
+extern template int vxprintf(printf_renderer<wchar_t>&,
+                             std::basic_string_ref<wchar_t>,
+                             va_list) noexcept;
+extern template int vxprintf(printf_renderer<char16_t>&,
+                             std::basic_string_ref<char16_t>,
+                             va_list) noexcept;
+extern template int vxprintf(printf_renderer<char32_t>&,
+                             std::basic_string_ref<char32_t>,
+                             va_list) noexcept;
 
 
 }} /* namespace __cxxabiv1::ext */
+
+#include <abi/ext/printf-inl.h>
 
 #endif /* _ABI_EXT_PRINTF_H_ */
