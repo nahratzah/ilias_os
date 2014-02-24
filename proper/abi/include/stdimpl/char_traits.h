20f9cb3f16f1987d76c65b3a86346c3f95ffa2cd
Ariane van der Steldt <ariane@stack.nl>
2014-02-24 02:16:51 +0000

    Lots of string/basic_string implementations.

--- proper/abi/include/stdimpl/char_traits.h
+++ proper/abi/include/stdimpl/char_traits.h
@@ -11,7 +11,7 @@ namespace impl {
  * implementation below.
  */
 template<typename CharT, typename FN = decltype(CharT::rfind)>
-auto rfind(const typename CharT::char_type, CharT>* s, size_t len,
+auto rfind(const typename CharT::char_type* s, size_t len,
            const typename CharT::char_type& c)
     noexcept(noexcept(CharT::rfind(s, len, c))) ->
     const typename CharT::char_type* {
@@ -19,12 +19,15 @@ auto rfind(const typename CharT::char_type, CharT>* s, size_t len,
 }
 
 template<typename CharT>
-auto rfind(basic_string_ref<typename CharT::char_type, CharT> s,
+auto rfind(const typename CharT::char_type* s, size_t len,
            const typename CharT::char_type& c, ...)
     noexcept ->
     const typename CharT::char_type* {
-  for (auto i = s.rbegin(); i != s.rend(); ++i)
-    if (CharT::eq(*i, c)) return &*i;
+  s += len;
+  while (len-- > 0) {
+    --s;
+    if (CharT::eq(*s, c)) return s;
+  }
   return nullptr;
 }
 
@@ -83,6 +86,168 @@ auto strrfind(const typename CharT::char_type* h, size_t hlen,
   do {
     if (CharT::compare(i, n, nlen) == 0) return i;
   } while (i-- != h);
+  return nullptr;
+}
+
+
+/*
+ * char_traits::strcspn wrapper:
+ * if char_traits::strcspn is defined, this function will invoke it.
+ * Otherwise, SFINAE will ignore this function and fall back to the
+ * implementation below.
+ */
+template<typename CharT, typename FN = decltype(CharT::strcspn)>
+auto strcspn(const typename CharT::char_type* h, size_t hlen,
+             const typename CharT::char_type* n, size_t nlen)
+    noexcept(noexcept(CharT::strcspn(h, hlen, n, nlen))) ->
+    const typename CharT::char_type* {
+  return CharT::strcspn(h, hlen, n, nlen);
+}
+
+template<typename CharT>
+auto strcspn(const typename CharT::char_type* h, size_t hlen,
+             const typename CharT::char_type* n, size_t nlen, ...)
+    noexcept ->
+    const typename CharT::char_type* {
+  const typename CharT::char_type* hi = h + hlen;
+  while (hlen-- > 0) {
+    if (CharT::find(n, nlen, *h) == nullptr) return h;
+    ++h;
+  }
+  return nullptr;
+}
+
+
+/*
+ * char_traits::strrcspn wrapper:
+ * if char_traits::strrcspn is defined, this function will invoke it.
+ * Otherwise, SFINAE will ignore this function and fall back to the
+ * implementation below.
+ */
+template<typename CharT, typename FN = decltype(CharT::strrcspn)>
+auto strrcspn(const typename CharT::char_type* h, size_t hlen,
+              const typename CharT::char_type* n, size_t nlen)
+    noexcept(noexcept(CharT::strrcspn(h, hlen, n, nlen))) ->
+    const typename CharT::char_type* {
+  return CharT::strrcspn(h, hlen, n, nlen);
+}
+
+template<typename CharT>
+auto strrcspn(const typename CharT::char_type* h, size_t hlen,
+              const typename CharT::char_type* n, size_t nlen, ...)
+    noexcept ->
+    const typename CharT::char_type* {
+  const typename CharT::char_type* hi = h + hlen;
+  while (hi-- != h)
+    if (CharT::find(n, nlen, *hi) == nullptr) return hi;
+  return nullptr;
+}
+
+
+/*
+ * char_traits::strspn wrapper:
+ * if char_traits::strspn is defined, this function will invoke it.
+ * Otherwise, SFINAE will ignore this function and fall back to the
+ * implementation below.
+ */
+template<typename CharT, typename FN = decltype(CharT::strspn)>
+auto strspn(const typename CharT::char_type* h, size_t hlen,
+             const typename CharT::char_type* n, size_t nlen)
+    noexcept(noexcept(CharT::strspn(h, hlen, n, nlen))) ->
+    const typename CharT::char_type* {
+  return CharT::strspn(h, hlen, n, nlen);
+}
+
+template<typename CharT>
+auto strspn(const typename CharT::char_type* h, size_t hlen,
+             const typename CharT::char_type* n, size_t nlen, ...)
+    noexcept ->
+    const typename CharT::char_type* {
+  const typename CharT::char_type* hi = h + hlen;
+  while (hlen-- > 0) {
+    if (CharT::find(n, nlen, *h) != nullptr) return h;
+    ++h;
+  }
+  return nullptr;
+}
+
+
+/*
+ * char_traits::strrspn wrapper:
+ * if char_traits::strrspn is defined, this function will invoke it.
+ * Otherwise, SFINAE will ignore this function and fall back to the
+ * implementation below.
+ */
+template<typename CharT, typename FN = decltype(CharT::strrspn)>
+auto strrspn(const typename CharT::char_type* h, size_t hlen,
+              const typename CharT::char_type* n, size_t nlen)
+    noexcept(noexcept(CharT::strrspn(h, hlen, n, nlen))) ->
+    const typename CharT::char_type* {
+  return CharT::strrspn(h, hlen, n, nlen);
+}
+
+template<typename CharT>
+auto strrspn(const typename CharT::char_type* h, size_t hlen,
+              const typename CharT::char_type* n, size_t nlen, ...)
+    noexcept ->
+    const typename CharT::char_type* {
+  const typename CharT::char_type* hi = h + hlen;
+  while (hi-- != h)
+    if (CharT::find(n, nlen, *hi) != nullptr) return hi;
+  return nullptr;
+}
+
+
+/*
+ * char_traits::cspn wrapper:
+ * if char_traits::cspn is defined, this function will invoke it.
+ * Otherwise, SFINAE will ignore this function and fall back to the
+ * implementation below.
+ */
+template<typename CharT, typename FN = decltype(CharT::cspn)>
+auto cspn(const typename CharT::char_type* s, size_t len,
+          const typename CharT::char_type& c)
+    noexcept(noexcept(CharT::cspn(s, len, c))) ->
+    const typename CharT::char_type* {
+  return CharT::spn(s, len, c);
+}
+
+template<typename CharT>
+auto spn(const typename CharT::char_type* s, size_t len,
+         const typename CharT::char_type& c, ...)
+    noexcept ->
+    const typename CharT::char_type* {
+  while (len-- > 0) {
+    if (!CharT::eq(*s, c)) return s;
+    ++s;
+  }
+  return nullptr;
+}
+
+/*
+ * char_traits::rcspn wrapper:
+ * if char_traits::rcspn is defined, this function will invoke it.
+ * Otherwise, SFINAE will ignore this function and fall back to the
+ * implementation below.
+ */
+template<typename CharT, typename FN = decltype(CharT::rcspn)>
+auto rspn(const typename CharT::char_type* s, size_t len,
+          const typename CharT::char_type& c)
+    noexcept(noexcept(CharT::rcspn(s, len, c))) ->
+    const typename CharT::char_type* {
+  return CharT::rspn(s, len, c);
+}
+
+template<typename CharT>
+auto rspn(const typename CharT::char_type* s, size_t len,
+          const typename CharT::char_type& c, ...)
+    noexcept ->
+    const typename CharT::char_type* {
+  s += len;
+  while (len-- > 0) {
+    --s;
+    if (!CharT::eq(*s, c)) return s;
+  }
   return nullptr;
 }
 
--- proper/abi/src/string.cc
+++ proper/abi/src/string.cc
@@ -6,9 +6,11 @@ _namespace_begin(std)
 
 
 template<>
-auto char_traits<char>::strfind(const char_type* haystack, size_t n_haystack,
-                                const char_type* needle, size_type n_needle)
-    noexcept {
+auto char_traits<char>::strfind(const char_type* haystack,
+                                size_t n_haystack,
+                                const char_type* needle,
+                                size_type n_needle)
+    noexcept -> const char_type* {
   return abi::ext::memfind(haystack, n_haystack, needle, n_needle);
 }
 
@@ -16,7 +18,8 @@ template<>
 auto char_traits<wchar_t>::strfind(const char_type* haystack,
                                    size_t n_haystack,
                                    const char_type* needle,
-                                   size_type n_needle) noexcept {
+                                   size_type n_needle)
+    noexcept -> const char_type* {
   return abi::ext::memfind(haystack, n_haystack, needle, n_needle);
 }
 
@@ -24,7 +27,8 @@ template<>
 auto char_traits<char16_t>::strfind(const char_type* haystack,
                                     size_t n_haystack,
                                     const char_type* needle,
-                                    size_type n_needle) noexcept {
+                                    size_type n_needle)
+    noexcept -> const char_type* {
   return abi::ext::memfind(haystack, n_haystack, needle, n_needle);
 }
 
@@ -32,8 +36,388 @@ template<>
 auto char_traits<char32_t>::strfind(const char_type* haystack,
                                     size_t n_haystack,
                                     const char_type* needle,
-                                    size_type n_needle) noexcept {
+                                    size_type n_needle)
+    noexcept -> const char_type* {
   return abi::ext::memfind(haystack, n_haystack, needle, n_needle);
+}
+
+
+template<>
+auto char_traits<char>::strrfind(const char_type* haystack,
+                                 size_t n_haystack,
+                                 const char_type* needle,
+                                 size_type n_needle)
+    noexcept -> const char_type* {
+  return abi::ext::memrfind(haystack, n_haystack, needle, n_needle);
+}
+
+template<>
+auto char_traits<wchar_t>::strrfind(const char_type* haystack,
+                                    size_t n_haystack,
+                                    const char_type* needle,
+                                    size_type n_needle)
+    noexcept -> const char_type* {
+  return abi::ext::memrfind(haystack, n_haystack, needle, n_needle);
+}
+
+template<>
+auto char_traits<char16_t>::strrfind(const char_type* haystack,
+                                     size_t n_haystack,
+                                     const char_type* needle,
+                                     size_type n_needle)
+    noexcept -> const char_type* {
+  return abi::ext::memrfind(haystack, n_haystack, needle, n_needle);
+}
+
+template<>
+auto char_traits<char32_t>::strrfind(const char_type* haystack,
+                                     size_t n_haystack,
+                                     const char_type* needle,
+                                     size_type n_needle)
+    noexcept -> const char_type* {
+  return abi::ext::memrfind(haystack, n_haystack, needle, n_needle);
+}
+
+
+template<>
+auto char_traits<char>::spn(const char_type* s, size_t len, char_type c)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_FORWARD;
+
+  auto r = reader<DIR_FORWARD, char_type>(s, len);
+  while (len-- > 0)
+    if (!eq(r.read8(len + 1U), c)) return r.addr<const char_type>() - 1;
+  return nullptr;
+}
+
+template<>
+auto char_traits<wchar_t>::spn(const char_type* s, size_t len, char_type c)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_FORWARD;
+
+  auto r = reader<DIR_FORWARD, char_type>(s, len);
+  while (len-- > 0)
+    if (!eq(r.read8(len + 1U), c)) return r.addr<const char_type>() - 1;
+  return nullptr;
+}
+
+template<>
+auto char_traits<char16_t>::spn(const char_type* s, size_t len, char_type c)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_FORWARD;
+
+  auto r = reader<DIR_FORWARD, char_type>(s, len);
+  while (len-- > 0)
+    if (!eq(r.read8(len + 1U), c)) return r.addr<const char_type>() - 1;
+  return nullptr;
+}
+
+template<>
+auto char_traits<char32_t>::spn(const char_type* s, size_t len, char_type c)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_FORWARD;
+
+  auto r = reader<DIR_FORWARD, char_type>(s, len);
+  while (len-- > 0)
+    if (!eq(r.read8(len + 1U), c)) return r.addr<const char_type>() - 1;
+  return nullptr;
+}
+
+
+template<>
+auto char_traits<char>::rspn(const char_type* s, size_t len, char_type c)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_BACKWARD;
+
+  auto r = reader<DIR_BACKWARD, char_type>(s + len, len);
+  while (len-- > 0)
+    if (!eq(r.read8(len + 1U), c)) return r.addr<const char_type>();
+  return nullptr;
+}
+
+template<>
+auto char_traits<wchar_t>::rspn(const char_type* s, size_t len, char_type c)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_BACKWARD;
+
+  auto r = reader<DIR_BACKWARD, char_type>(s + len, len);
+  while (len-- > 0)
+    if (!eq(r.read8(len + 1U), c)) return r.addr<const char_type>();
+  return nullptr;
+}
+
+template<>
+auto char_traits<char16_t>::rspn(const char_type* s, size_t len, char_type c)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_BACKWARD;
+
+  auto r = reader<DIR_BACKWARD, char_type>(s + len, len);
+  while (len-- > 0)
+    if (!eq(r.read8(len + 1U), c)) return r.addr<const char_type>();
+  return nullptr;
+}
+
+template<>
+auto char_traits<char32_t>::rspn(const char_type* s, size_t len, char_type c)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_BACKWARD;
+
+  auto r = reader<DIR_BACKWARD, char_type>(s + len, len);
+  while (len-- > 0)
+    if (!eq(r.read8(len + 1U), c)) return r.addr<const char_type>();
+  return nullptr;
+}
+
+
+template<>
+auto char_traits<char>::strspn(const char_type* h, size_t hlen,
+                               const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_FORWARD;
+
+  auto r = reader<DIR_FORWARD, char_type>(s, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) == nullptr)
+      return r.addr<const char_type*>() - 1;
+  }
+  return nullptr;
+}
+
+template<>
+auto char_traits<wchar_t>::strspn(const char_type* h, size_t hlen,
+                                  const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_FORWARD;
+
+  auto r = reader<DIR_FORWARD, char_type>(s, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) == nullptr)
+      return r.addr<const char_type*>() - 1;
+  }
+  return nullptr;
+}
+
+template<>
+auto char_traits<char16_t>::strspn(const char_type* h, size_t hlen,
+                                   const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_FORWARD;
+
+  auto r = reader<DIR_FORWARD, char_type>(s, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) == nullptr)
+      return r.addr<const char_type*>() - 1;
+  }
+  return nullptr;
+}
+
+template<>
+auto char_traits<char32_t>::strspn(const char_type* h, size_t hlen,
+                                   const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_FORWARD;
+
+  auto r = reader<DIR_FORWARD, char_type>(s, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) == nullptr)
+      return r.addr<const char_type*>() - 1;
+  }
+  return nullptr;
+}
+
+
+template<>
+auto char_traits<char>::strrspn(const char_type* h, size_t hlen,
+                                const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_BACKWARD;
+
+  auto r = reader<DIR_BACKWARD, char_type>(s + len, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) == nullptr)
+      return r.addr<const char_type*>();
+  }
+  return nullptr;
+}
+
+template<>
+auto char_traits<wchar_t>::strrspn(const char_type* h, size_t hlen,
+                                   const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_BACKWARD;
+
+  auto r = reader<DIR_BACKWARD, char_type>(s + len, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) == nullptr)
+      return r.addr<const char_type*>();
+  }
+  return nullptr;
+}
+
+template<>
+auto char_traits<char16_t>::strrspn(const char_type* h, size_t hlen,
+                                    const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_BACKWARD;
+
+  auto r = reader<DIR_BACKWARD, char_type>(s + len, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) == nullptr)
+      return r.addr<const char_type*>();
+  }
+  return nullptr;
+}
+
+template<>
+auto char_traits<char32_t>::strrspn(const char_type* h, size_t hlen,
+                                    const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_BACKWARD;
+
+  auto r = reader<DIR_BACKWARD, char_type>(s + len, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) == nullptr)
+      return r.addr<const char_type*>();
+  }
+  return nullptr;
+}
+
+
+template<>
+auto char_traits<char>::strcspn(const char_type* h, size_t hlen,
+                                const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_FORWARD;
+
+  auto r = reader<DIR_FORWARD, char_type>(s, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) != nullptr)
+      return r.addr<const char_type*>() - 1;
+  }
+  return nullptr;
+}
+
+template<>
+auto char_traits<wchar_t>::strcspn(const char_type* h, size_t hlen,
+                                   const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_FORWARD;
+
+  auto r = reader<DIR_FORWARD, char_type>(s, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) != nullptr)
+      return r.addr<const char_type*>() - 1;
+  }
+  return nullptr;
+}
+
+template<>
+auto char_traits<char16_t>::strcspn(const char_type* h, size_t hlen,
+                                    const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_FORWARD;
+
+  auto r = reader<DIR_FORWARD, char_type>(s, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) != nullptr)
+      return r.addr<const char_type*>() - 1;
+  }
+  return nullptr;
+}
+
+template<>
+auto char_traits<char32_t>::strcspn(const char_type* h, size_t hlen,
+                                    const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_FORWARD;
+
+  auto r = reader<DIR_FORWARD, char_type>(s, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) != nullptr)
+      return r.addr<const char_type*>() - 1;
+  }
+  return nullptr;
+}
+
+
+template<>
+auto char_traits<char>::strrcspn(const char_type* h, size_t hlen,
+                                 const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_BACKWARD;
+
+  auto r = reader<DIR_BACKWARD, char_type>(s + len, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) != nullptr)
+      return r.addr<const char_type*>();
+  }
+  return nullptr;
+}
+
+template<>
+auto char_traits<wchar_t>::strrcspn(const char_type* h, size_t hlen,
+                                    const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_BACKWARD;
+
+  auto r = reader<DIR_BACKWARD, char_type>(s + len, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) != nullptr)
+      return r.addr<const char_type*>();
+  }
+  return nullptr;
+}
+
+template<>
+auto char_traits<char16_t>::strrcspn(const char_type* h, size_t hlen,
+                                     const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_BACKWARD;
+
+  auto r = reader<DIR_BACKWARD, char_type>(s + len, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) != nullptr)
+      return r.addr<const char_type*>();
+  }
+  return nullptr;
+}
+
+template<>
+auto char_traits<char32_t>::strrcspn(const char_type* h, size_t hlen,
+                                     const char_type* n, size_t nlen)
+    noexcept -> const char_type* {
+  using abi::ext::reader;
+  using abi::ext::DIR_BACKWARD;
+
+  auto r = reader<DIR_BACKWARD, char_type>(s + len, len);
+  while (len-- > 0) {
+    if (find(n, nlen, r.read8(len + 1U)) != nullptr)
+      return r.addr<const char_type*>();
+  }
+  return nullptr;
 }
 
 
