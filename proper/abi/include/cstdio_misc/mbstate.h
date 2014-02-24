242b59e41eddbeb50818b5b2768285e3f39e6aa0
Ariane van der Steldt <ariane@stack.nl>
2014-02-24 02:17:57 +0000

    Extract mbstate_t.

---
+++ proper/abi/include/cstdio_misc/mbstate.h
@@ -0,0 +1,12 @@
+#ifndef _CSTDIO_MISC_MBSTATE_H_
+#define _CSTDIO_MISC_MBSTATE_H_
+
+#include <abi/abi.h>
+
+_namespace_begin(std)
+
+typedef _TYPES(mbstate_t) mbstate_t;
+
+_namespace_end(std)
+
+#endif /* _CSTDIO_MISC_MBSTATE_H_ */
