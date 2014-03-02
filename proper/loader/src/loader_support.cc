a1adf5032f49704f49b77b682d8a7281d3cdf6e5
Ariane van der Steldt <ariane@stack.nl>
2014-03-02 20:00:54 +0000

    Run global constructors/destructors.

--- proper/loader/src/loader_support.cc
+++ proper/loader/src/loader_support.cc
@@ -2,6 +2,7 @@
 #include <abi/abi.h>
 #include <abi/memory.h>
 #include <loader/x86_video.h>
+#include <iterator>
 
 namespace loader {
 
@@ -19,6 +20,28 @@ void bss_zero() noexcept {
 
   abi::memzero(&sbss, ebss - sbss);
   bios_put_str("BSS cleared.\n");
+}
+
+
+namespace {
+
+using ctor_dtor = void (*)();  /* Global constructor/destructor. */
+
+} /* namespace loader::<unnamed> */
+
+
+void loader_constructors() noexcept {
+  extern ctor_dtor start_ctors, end_ctors;  /* Provided by linker. */
+
+  ctor_dtor* i = &start_ctors;
+  while (i != &end_ctors) (*i++)();
+}
+
+void loader_destructors() noexcept {
+  extern ctor_dtor start_dtors, end_dtors;  /* Provided by linker. */
+
+  ctor_dtor* i = &end_dtors;
+  while (i-- != &start_dtors) (*i)();
 }
 
 _cdecl_end
