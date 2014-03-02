63bfc84ce8a914bcb55e68aaffb4071d23b538fe
Ariane van der Steldt <ariane@stack.nl>
2014-03-02 19:42:21 +0000

    Add string_ref constructor to stdexcept.

--- proper/abi/src/stdexcept.cc
+++ proper/abi/src/stdexcept.cc
@@ -4,12 +4,16 @@
 namespace std {
 
 
+logic_error::logic_error(string_ref msg)
+: msg_(abi::ext::make_c_string_ptr(msg.data(), msg.length()))
+{}
+
 logic_error::logic_error(const string& msg)
-: msg_(abi::ext::make_c_string_ptr(msg.c_str(), msg.length()))
+: logic_error(string_ref(msg))
 {}
 
 logic_error::logic_error(const char* msg)
-: msg_(abi::ext::make_c_string_ptr(msg))
+: logic_error(string_ref(msg))
 {}
 
 logic_error::logic_error(const logic_error& other) noexcept
@@ -31,12 +35,16 @@ const char* logic_error::what() const noexcept {
 }
 
 
+runtime_error::runtime_error(string_ref msg)
+: msg_(abi::ext::make_c_string_ptr(msg.data(), msg.length()))
+{}
+
 runtime_error::runtime_error(const string& msg)
-: msg_(abi::ext::make_c_string_ptr(msg.c_str(), msg.length()))
+: runtime_error(string_ref(msg))
 {}
 
 runtime_error::runtime_error(const char* msg)
-: msg_(abi::ext::make_c_string_ptr(msg))
+: runtime_error(string_ref(msg))
 {}
 
 runtime_error::runtime_error(const runtime_error& other) noexcept
@@ -57,30 +65,37 @@ const char* runtime_error::what() const noexcept {
 }
 
 
+domain_error::domain_error(string_ref msg) : logic_error(msg) {}
 domain_error::domain_error(const string& msg) : logic_error(msg) {}
 domain_error::domain_error(const char* msg) : logic_error(msg) {}
 domain_error::~domain_error() noexcept {}
 
+invalid_argument::invalid_argument(string_ref msg) : logic_error(msg) {}
 invalid_argument::invalid_argument(const string& msg) : logic_error(msg) {}
 invalid_argument::invalid_argument(const char* msg) : logic_error(msg) {}
 invalid_argument::~invalid_argument() noexcept {}
 
+length_error::length_error(string_ref msg) : logic_error(msg) {}
 length_error::length_error(const string& msg) : logic_error(msg) {}
 length_error::length_error(const char* msg) : logic_error(msg) {}
 length_error::~length_error() noexcept {}
 
+out_of_range::out_of_range(string_ref msg) : logic_error(msg) {}
 out_of_range::out_of_range(const string& msg) : logic_error(msg) {}
 out_of_range::out_of_range(const char* msg) : logic_error(msg) {}
 out_of_range::~out_of_range() noexcept {}
 
+range_error::range_error(string_ref msg) : runtime_error(msg) {}
 range_error::range_error(const string& msg) : runtime_error(msg) {}
 range_error::range_error(const char* msg) : runtime_error(msg) {}
 range_error::~range_error() noexcept {}
 
+overflow_error::overflow_error(string_ref msg) : runtime_error(msg) {}
 overflow_error::overflow_error(const string& msg) : runtime_error(msg) {}
 overflow_error::overflow_error(const char* msg) : runtime_error(msg) {}
 overflow_error::~overflow_error() noexcept {}
 
+underflow_error::underflow_error(string_ref msg) : runtime_error(msg) {}
 underflow_error::underflow_error(const string& msg) : runtime_error(msg) {}
 underflow_error::underflow_error(const char* msg) : runtime_error(msg) {}
 underflow_error::~underflow_error() noexcept {}
