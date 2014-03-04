3934557411038ef757cb57e1596fe2d93d9e4d8b
Ariane van der Steldt <ariane@stack.nl>
2014-03-04 22:38:31 +0000

    Link in loader panic routine.

--- proper/loader/src/loader_panic.cc
+++ proper/loader/src/loader_panic.cc
@@ -4,15 +4,15 @@
 
 namespace loader {
 
-void panic(const char* fmt, ...) noexcept __attribute__((noreturn)) {
+void panic(const char* fmt, ...) noexcept {
   va_list ap;
 
   va_start(ap, fmt);
-  vpanic(string_ref(fmt), ap);
+  vpanic(std::string_ref(fmt), ap);
   va_end(ap);
 }
 
-void panic(std::string_ref fmt, ...) noexcept __attribute__((noreturn)) {
+void panic(std::string_ref fmt, ...) noexcept {
   va_list ap;
 
   va_start(ap, fmt);
@@ -20,15 +20,13 @@ void panic(std::string_ref fmt, ...) noexcept __attribute__((noreturn)) {
   va_end(ap);
 }
 
-void vpanic(const char* fmt, va_list ap)
-    noexcept __attribute__((noreturn)) {
-  vpanic(string_ref(fmt), ap);
+void vpanic(const char* fmt, va_list ap) noexcept {
+  vpanic(std::string_ref(fmt), ap);
 }
 
-void vpanic(std::string_ref fmt, va_list ap)
-    noexcept __attribute__((noreturn)) {
+void vpanic(std::string_ref fmt, va_list ap) noexcept {
   bios_put_str("loader panic: ");
-  bios_put_str(fmt, ap);
+  bios_vprintf(fmt, ap);
 
   for (;;) {
 #if defined(__i386__) || defined(__amd64__) || defined(__x86_64__)
