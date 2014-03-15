6b67ff2be81d43ddb7e143e389cdb774c2dcc9d9
Ariane van der Steldt <ariane@stack.nl>
2014-03-15 05:05:56 +0000

    Precompile vectors for all builtin types.

---
+++ proper/abi/src/vector.cc
@@ -0,0 +1,20 @@
+#include <vector>
+
+_namespace_begin(std)
+
+
+template class vector<char>;
+template class vector<wchar_t>;
+template class vector<unsigned char>;
+template class vector<signed char>;
+template class vector<unsigned short>;
+template class vector<short>;
+template class vector<unsigned int>;
+template class vector<int>;
+template class vector<unsigned long>;
+template class vector<long>;
+template class vector<unsigned long long>;
+template class vector<long long>;
+
+
+_namespace_end(std)
