b8989b2d521b544cf2d38d505adca9c9e37069b3
Ariane van der Steldt <ariane@stack.nl>
2014-04-11 23:22:10 +0100

    Add inclusion guard to loader abi.

--- proper/loader/include/loader/abi.h
+++ proper/loader/include/loader/abi.h
@@ -1,3 +1,6 @@
+#ifndef _LOADER_ABI_H_
+#define _LOADER_ABI_H_
+
 #include <abi/abi.h>
 #include <tuple>
 
@@ -9,3 +12,5 @@ _namespace(std)::tuple<void*, _TYPES(size_t)> heap_malloc(_TYPES(size_t))
 constexpr bool heap_free(void*, _TYPES(size_t)) noexcept { return false; }
 
 } /* namespace loader */
+
+#endif /* _LOADER_ABI_H_ */
