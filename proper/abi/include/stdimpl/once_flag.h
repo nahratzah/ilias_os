87e42ef2e4e20016422629def3a02eaab248edc2
Ariane van der Steldt <ariane@stack.nl>
2014-04-21 17:07:44 +0100

    Simplify once_flag.

--- proper/abi/include/stdimpl/once_flag.h
+++ proper/abi/include/stdimpl/once_flag.h
@@ -4,6 +4,7 @@
 #include <cdecl.h>
 #ifdef __cplusplus
 # include <atomic>
+# include <type_traits>
 #endif
 #include <cstdbool>
 
@@ -11,13 +12,17 @@ _namespace_begin(std)
 
 struct once_flag {
 #ifdef __cplusplus
-  atomic<bool> lock = { false };
-  volatile bool done = false;
+  atomic<bool> lock;
+  volatile bool done;
 #else
-  _Atomic(Bool) lock = { false };
-  volatile Bool done = false;
+  _Atomic(Bool) lock;
+  volatile Bool done;
 #endif
 };
+
+#ifdef __cplusplus
+static_assert(is_pod<once_flag>::value, "Once flag should be a POD type.");
+#endif
 
 _namespace_end(std)
 
