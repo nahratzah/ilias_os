20b68a5f57c4e1f686b25f38721aa2fccedfddd4
Ariane van der Steldt <ariane@stack.nl>
2014-05-03 18:27:32 +0100

    Implement lock_guard, unique_lock.

--- proper/abi/include/mutex-inl.h
+++ proper/abi/include/mutex-inl.h
@@ -4,6 +4,7 @@
 #include <atomic>
 #include <utility>
 #include <abi/ext/atomic.h>
+#include <system_error>
 
 _namespace_begin(std)
 
@@ -54,6 +55,169 @@ void call_once(once_flag& flag, Callable&& func, Args&&... args)
     func(forward<Args>(args)...);
     lck.commit();
   }
+}
+
+
+template<typename Mutex>
+lock_guard<Mutex>::lock_guard(mutex_type& m)
+: m_(&m)
+{
+  m_.lock();
+}
+
+template<typename Mutex>
+lock_guard<Mutex>::lock_guard(mutex_type& m, adopt_lock_t)
+: m_(&m)
+{}
+
+template<typename Mutex>
+lock_guard<Mutex>::~lock_guard() noexcept {
+  m_.unlock();
+}
+
+
+template<typename Mutex>
+unique_lock<Mutex>::unique_lock(mutex_type& m)
+: m_(&m)
+{
+  m_->lock();
+  locked_ = true;
+}
+
+template<typename Mutex>
+unique_lock<Mutex>::unique_lock(mutex_type& m, defer_lock_t) noexcept
+: m_(&m)
+{}
+
+template<typename Mutex>
+unique_lock<Mutex>::unique_lock(mutex_type& m, try_to_lock_t)
+: m_(&m),
+  locked_(m_->try_lock())
+{}
+
+template<typename Mutex>
+unique_lock<Mutex>::unique_lock(mutex_type& m, adopt_lock_t)
+: m_(&m),
+  locked_(true)
+{}
+
+#if 0 // XXX implement chrono for this
+template<typename Mutex>
+template<typename Clock, typename Duration>
+unique_lock<Mutex>::unique_lock(
+    mutex_type& m, const chrono::time_point<Clock, Duration>& abs_time)
+: m_(&m),
+  locked_(m_->try_lock_until(abs_time))
+{}
+
+template<typename Mutex>
+template<typename Rep, typename Period>
+unique_lock<Mutex>::unique_lock(mutex_type& m,
+                                const chrono::duration<Rep, Period>& rel_time)
+: m_(&m),
+  locked_(m_->try_lock_for(rel_time))
+{}
+#endif
+
+template<typename Mutex>
+unique_lock<Mutex>::unique_lock(unique_lock&& o) noexcept
+: m_(exchange(o.m_, nullptr)),
+  locked_(exchange(o.locked_, false))
+{}
+
+template<typename Mutex>
+auto unique_lock<Mutex>::operator=(unique_lock&& o) noexcept -> unique_lock& {
+  unique_lock(move(o)).swap(*this);
+}
+
+template<typename Mutex>
+unique_lock<Mutex>::~unique_lock() noexcept {
+  if (locked_) m_->unlock();
+}
+
+template<typename Mutex>
+auto unique_lock<Mutex>::lock() -> void {
+  if (_predict_false(m_ == nullptr))
+    throw system_error(make_error_code(errc::operation_not_permitted));
+  if (_predict_false(locked_))
+    throw system_error(make_error_code(errc::resource_deadlock_would_occur));
+  m_->lock();
+  locked_ = true;
+}
+
+template<typename Mutex>
+auto unique_lock<Mutex>::try_lock() -> bool {
+  if (_predict_false(m_ == nullptr))
+    throw system_error(make_error_code(errc::operation_not_permitted));
+  if (_predict_false(locked_))
+    throw system_error(make_error_code(errc::resource_deadlock_would_occur));
+  return locked_ = m_->try_lock();
+}
+
+#if 0 // XXX implement chrono for this
+template<typename Mutex>
+template<typename Clock, typename Duration>
+auto unique_lock<Mutex>::try_lock_until(
+    const chrono::time_point<Clock, Duration>& abs_time) noexcept -> bool {
+  if (_predict_false(m_ == nullptr))
+    throw system_error(make_error_code(errc::operation_not_permitted));
+  if (_predict_false(locked_))
+    throw system_error(make_error_code(errc::resource_deadlock_would_occur));
+  return locked_ = m_->try_lock_until(abs_time);
+}
+
+template<typename Mutex>
+template<typename Rep, typename Period>
+auto unique_lock<Mutex>::try_lock_for(
+    const chrono::duration<Rep, Period>& rel_time) noexcept -> bool {
+  if (_predict_false(m_ == nullptr))
+    throw system_error(make_error_code(errc::operation_not_permitted));
+  if (_predict_false(locked_))
+    throw system_error(make_error_code(errc::resource_deadlock_would_occur));
+  return locked_ = m_->try_lock_for(rel_time);
+}
+#endif
+
+template<typename Mutex>
+auto unique_lock<Mutex>::unlock() -> void {
+  if (_predict_false(!locked_))
+    throw system_error(make_error_code(errc::operation_not_permitted));
+  m_->unlock();
+  locked_ = false;
+}
+
+template<typename Mutex>
+auto unique_lock<Mutex>::swap(unique_lock& o) noexcept -> void {
+  using _namespace(std)::swap;
+
+  swap(m_, o.m_);
+  swap(locked_, o.locked_);
+}
+
+template<typename Mutex>
+auto unique_lock<Mutex>::release() noexcept -> mutex_type* {
+  locked_ = false;
+  return exchange(m_, nullptr);
+}
+
+template<typename Mutex>
+auto unique_lock<Mutex>::owns_lock() const noexcept -> bool {
+  return locked_;
+}
+
+template<typename Mutex>
+unique_lock<Mutex>::operator bool() const noexcept {
+  return locked_;
+}
+
+template<typename Mutex>
+auto unique_lock<Mutex>::mutex() const noexcept -> mutex_type* {
+  return m_;
+}
+
+template<typename Mutex>
+void swap(unique_lock<Mutex>& a, unique_lock<Mutex>& b) noexcept {
+  a.swap(b);
 }
 
 
