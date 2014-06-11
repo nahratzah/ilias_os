54bf9951ef5ee56d34cfd9bf577a82034d4d7d8b
Ariane van der Steldt <ariane@stack.nl>
2014-06-11 22:18:25 +0100

    Remove hard-coded std:: namespace, using implicit local namespace instead.

--- proper/abi/include/functional-inl.h
+++ proper/abi/include/functional-inl.h
@@ -748,7 +748,7 @@ template<typename R, typename... ArgTypes>
 template<typename F>
 auto function<R(ArgTypes...)>::data_type::assign(bool is_special,
                                                  F&& f) -> fn_type {
-  return assign(is_special, forward<F>(f), std::allocator<void>());
+  return assign(is_special, forward<F>(f), allocator<void>());
 }
 
 template<typename R, typename... ArgTypes>
