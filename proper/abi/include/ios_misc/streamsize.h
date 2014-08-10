0aa77d9fb4226a66e6a6af30a5f5d28fd1a3d96a
Ariane van der Steldt <ariane@stack.nl>
2014-08-10 18:57:45 +0100

    Implement wbuffer_convert.

---
+++ proper/abi/include/ios_misc/streamsize.h
@@ -0,0 +1,13 @@
+#ifndef _IOS_MISC_STREAMSIZE_H_
+#define _IOS_MISC_STREAMSIZE_H_
+
+#include <cdecl.h>
+#include <abi/abi.h>
+
+_namespace_begin(std)
+
+using streamsize = _TYPES(fpos_t);
+
+_namespace_end(std)
+
+#endif /* _IOS_MISC_STREAMSIZE_H_ */
--- proper/abi/include/locale-inl.h
+++ proper/abi/include/locale-inl.h
@@ -6,6 +6,7 @@
 #include <ios>
 #include <string>
 #include <stdimpl/locale_num_put.h>
+#include <streambuf>
 
 #ifndef _LOCALE_INLINE
 # define _LOCALE_INLINE  extern inline
@@ -349,6 +350,240 @@ auto wstring_convert<Codecvt, Elem, WA, BA>::converted() const -> size_t {
 template<typename Codecvt, typename Elem, typename WA, typename BA>
 auto wstring_convert<Codecvt, Elem, WA, BA>::state() const -> state_type {
   return cvt_state_;
+}
+
+
+template<typename Codecvt, typename Elem, typename Tr>
+wbuffer_convert<Codecvt, Elem, Tr>::wbuffer_convert(streambuf* buf,
+                                                    Codecvt* cvt,
+                                                    state_type cvt_state)
+: buf_(buf),
+  cvt_(cvt),
+  cvt_state_(cvt_state),
+  always_noconv_(cvt->always_noconv())
+{}
+
+template<typename Codecvt, typename Elem, typename Tr>
+auto wbuffer_convert<Codecvt, Elem, Tr>::rdbuf() const -> streambuf* {
+  return buf_;
+}
+
+template<typename Codecvt, typename Elem, typename Tr>
+auto wbuffer_convert<Codecvt, Elem, Tr>::rdbuf(streambuf* buf) -> streambuf* {
+  return exchange(buf_, buf);
+}
+
+template<typename Codecvt, typename Elem, typename Tr>
+auto wbuffer_convert<Codecvt, Elem, Tr>::state() const -> state_type {
+  return cvt_state_;
+}
+
+template<typename Codecvt, typename Elem, typename Tr>
+auto wbuffer_convert<Codecvt, Elem, Tr>::showmanyc() ->
+    streamsize {
+  return -1;
+}
+
+template<typename Codecvt, typename Elem, typename Tr>
+auto wbuffer_convert<Codecvt, Elem, Tr>::xsgetn(
+    typename basic_streambuf<Elem, Tr>::char_type* s, streamsize n) ->
+    streamsize {
+  using traits_type = typename basic_streambuf<Elem, Tr>::traits_type;
+  using int_type = typename basic_streambuf<Elem, Tr>::int_type;
+  using char_type = typename basic_streambuf<Elem, Tr>::char_type;
+
+  if (!buf_) return 0;  // XXX exception?
+  if (_predict_false(always_noconv_)) return buf_->sgetn(s, n);
+  char_type*const e = s + n;
+
+  streamsize read_count = 0;
+  while (n > 0) {
+    const int_type cc;
+    bool succes;
+    try {
+      tie(cc, ignore, succes) = uflow();
+    } catch (...) {
+      if (read_count == 0) throw;
+      break;
+    }
+
+    if (traits_type::eq_int_type(cc, traits_type::eof())) break;
+    *s = traits_type::to_char_type(cc);
+
+    ++read_count;
+    ++s;
+    --n;
+  }
+  return read_count;
+}
+
+template<typename Codecvt, typename Elem, typename Tr>
+auto wbuffer_convert<Codecvt, Elem, Tr>::underflow() ->
+    typename basic_streambuf<Elem, Tr>::int_type {
+  typename basic_streambuf<Elem, Tr>::int_type rv;
+  bool succes;
+  tie(rv, ignore, succes) = underflow_(false);
+  if (!succes) throw range_error("wbuffer_convert: failed to convert input");
+  return rv;
+}
+
+template<typename Codecvt, typename Elem, typename Tr>
+auto wbuffer_convert<Codecvt, Elem, Tr>::uflow() ->
+    typename basic_streambuf<Elem, Tr>::int_type {
+  typename basic_streambuf<Elem, Tr>::int_type rv;
+  bool succes;
+  tie(rv, ignore, succes) = underflow_(true);
+  if (!succes) throw range_error("wbuffer_convert: failed to convert input");
+  return rv;
+}
+
+template<typename Codecvt, typename Elem, typename Tr>
+auto wbuffer_convert<Codecvt, Elem, Tr>::xsputn(
+    const typename basic_streambuf<Elem, Tr>::char_type* s, streamsize n) ->
+    streamsize {
+  using traits_type = typename basic_streambuf<Elem, Tr>::traits_type;
+  using char_type = typename basic_streambuf<Elem, Tr>::char_type;
+
+  if (!buf_) return 0;  // XXX exception?
+  const char_type* s_end = s + n;
+  const char_type* s_next = s;
+
+  /* Convert and write entire string. */
+  while (s_next != s_end) {
+    array<streambuf::char_type, 64> tmp;
+    auto out_next = begin(tmp);
+
+    /* Conversion into temporary buffer. */
+    auto cvt_rv = cvt_->out(cvt_state_,
+                            s_next, s_end, s_next,
+                            tmp.begin(), tmp.end(), out_next);
+    /* Copy temporary buffer to underlying streambuf. */
+    auto out_begin = tmp.begin();
+    while (out_begin != out_next) {
+      const streamsize wcount = buf_->sputn(out_begin, out_next - out_begin);
+      out_begin += wcount;
+      if (wcount == 0) {
+        if (!streambuf::traits_type::eq_int_type(streambuf::traits_type::eof(),
+                                                 buf_->sputc(*out_begin)))
+          ++out_begin;
+        else  // XXX
+          assert_msg(false, "No idea how to break out of infinite loop...");
+      }
+    }
+
+    /* Handle conversion result. */
+    switch (cvt_rv) {
+    case codecvt_base::ok:
+    case codecvt_base::partial:
+      break;
+    case codecvt_base::error:
+      if (s_next == s)
+        throw range_error("wbuffer_convert: failed to convert output");
+      return s_next - s;
+      break;
+    case codecvt_base::noconv:
+      /* No conversion required: copy immediately. */
+      while (s_next != s_end) {
+        if (streambuf::traits_type::eq_int_type(streambuf::traits_type::eof(),
+                                                buf_->sputc(*s_next)))
+          break;
+        ++s_next;
+      }
+      return s_next - s;
+    }
+  }
+
+  return s_next - s;
+}
+
+template<typename Codecvt, typename Elem, typename Tr>
+auto wbuffer_convert<Codecvt, Elem, Tr>::overflow(
+    typename basic_streambuf<Elem, Tr>::int_type cc) ->
+    typename basic_streambuf<Elem, Tr>::int_type {
+  using traits_type = typename basic_streambuf<Elem, Tr>::traits_type;
+  using char_type = typename basic_streambuf<Elem, Tr>::char_type;
+
+  if (!traits_type::eq_int_type(cc, traits_type::eof())) {
+    char_type c = traits_type::to_char_type(cc);
+    sputn(&c, 1);
+  }
+  return cc;
+}
+
+template<typename Codecvt, typename Elem, typename Tr>
+auto wbuffer_convert<Codecvt, Elem, Tr>::underflow_(bool do_bump) ->
+    tuple<typename basic_streambuf<Elem, Tr>::int_type, streamsize, bool> {
+  using traits_type = typename basic_streambuf<Elem, Tr>::traits_type;
+  using int_type = typename basic_streambuf<Elem, Tr>::int_type;
+  using char_type = typename basic_streambuf<Elem, Tr>::char_type;
+
+  if (!buf_) return { traits_type::eof(), 0, false };  // XXX exception?
+
+  /* Set up result values. */
+  streamsize bumpcount = 0;
+  char_type result;
+  auto out_next = &result;
+  const auto out_e = &result + 1;
+
+  /* Allow loop to first clean out pending out chars in codecvt. */
+  bool do_read = false;
+
+  /* Operate on a copy of streamstate. */
+  state_type state_ = this->state_;
+
+  /* Read and convert from underlying buffer. */
+  while (out_next != out_e) {
+    typename streambuf::int_type cc;
+    if (do_read) {
+      /* Read 1 byte; increments position regardless
+       * (will undo position increment afterwards, iff !do_bump). */
+      cc = buf_->sbumpc();
+
+      /* Handle eof. */
+      if (streambuf::traits_type::eq_int_type(cc,
+                                              streambuf::traits_type::eof())) {
+        for (auto i = bumpcount; i > 0; --i) buf_->sungetc();
+        return { traits_type::eof(), 0, bumpcount == 0 };
+      }
+
+      ++bumpcount;
+    } else {
+      cc = 0;
+    }
+
+    /* Convert read character, which may yield an output character. */
+    typename streambuf::char_type c = streambuf::traits_type::to_char_type(cc);
+    auto in_next = &c;
+    const auto in_e = &c + (do_read ? 1 : 0);
+    auto cvt_result = cvt_->in(cvt_state_,
+                               in_next, in_e, in_next,
+                               out_next, out_e, out_next);
+
+    /* Handle conversion result. */
+    switch(cvt_result) {
+    case codecvt_base::ok:
+    case codecvt_base::partial:
+      break;
+    case codecvt_base::error:
+      for (auto i = bumpcount; i > 0; --i) buf_->sungetc();
+      return { traits_type::eof(), 0, false };
+    case codecvt_base::noconv:
+      result = c;
+      ++out_next;
+      break;
+    }
+
+    do_read = true;
+  }
+
+  /* Undo bumping, if we were not supposed to do that. */
+  if (!do_bump)
+    for (auto i = bumpcount; i > 0; --i) buf_->sungetc();
+
+  /* Save state. */
+  this->state_ = state_;
+
+  return { traits_type::to_int_type(result), bumpcount, true };
 }
 
 
