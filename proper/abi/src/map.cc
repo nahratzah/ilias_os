0b66ba9da7655ac27424c884f1216f69012b7ab9
Ariane van der Steldt <ariane@stack.nl>
2014-08-17 16:33:25 +0100

    Implement std::map.

---
+++ proper/abi/src/map.cc
@@ -0,0 +1,19 @@
+#include <map>
+
+_namespace_begin(std)
+
+
+template class map<char, char>;
+template class map<signed char, signed char>;
+template class map<unsigned char, unsigned char>;
+template class map<short, short>;
+template class map<unsigned short, unsigned short>;
+template class map<int, int>;
+template class map<unsigned int, unsigned int>;
+template class map<long, long>;
+template class map<unsigned long, unsigned long>;
+template class map<long long, long long>;
+template class map<unsigned long long, unsigned long long>;
+
+
+_namespace_end(std)
