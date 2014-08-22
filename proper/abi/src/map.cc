51773afe1e5bb6d87d6b011446973c46bfbac1bf
Ariane van der Steldt <ariane@stack.nl>
2014-08-22 14:55:55 +0100

    Export operator==, operator< for std::map.

--- proper/abi/src/map.cc
+++ proper/abi/src/map.cc
@@ -15,5 +15,95 @@ template class map<unsigned long, unsigned long>;
 template class map<long long, long long>;
 template class map<unsigned long long, unsigned long long>;
 
+template bool operator==(const map<char,
+                                   char>&,
+                         const map<char,
+                                   char>&);
+template bool operator==(const map<signed char,
+                                   signed char>&,
+                         const map<signed char,
+                                   signed char>&);
+template bool operator==(const map<unsigned char,
+                                   unsigned char>&,
+                         const map<unsigned char,
+                                   unsigned char>&);
+template bool operator==(const map<short,
+                                   short>&,
+                         const map<short,
+                                   short>&);
+template bool operator==(const map<unsigned short,
+                                   unsigned short>&,
+                         const map<unsigned short,
+                                   unsigned short>&);
+template bool operator==(const map<int,
+                                   int>&,
+                         const map<int,
+                                   int>&);
+template bool operator==(const map<unsigned int,
+                                   unsigned int>&,
+                         const map<unsigned int,
+                                   unsigned int>&);
+template bool operator==(const map<long,
+                                   long>&,
+                         const map<long,
+                                   long>&);
+template bool operator==(const map<unsigned long,
+                                   unsigned long>&,
+                         const map<unsigned long,
+                                   unsigned long>&);
+template bool operator==(const map<long long,
+                                   long long>&,
+                         const map<long long,
+                                   long long>&);
+template bool operator==(const map<unsigned long long,
+                                   unsigned long long>&,
+                         const map<unsigned long long,
+                                   unsigned long long>&);
+
+template bool operator<(const map<char,
+                                  char>&,
+                        const map<char,
+                                  char>&);
+template bool operator<(const map<signed char,
+                                  signed char>&,
+                        const map<signed char,
+                                  signed char>&);
+template bool operator<(const map<unsigned char,
+                                  unsigned char>&,
+                        const map<unsigned char,
+                                  unsigned char>&);
+template bool operator<(const map<short,
+                                  short>&,
+                        const map<short,
+                                  short>&);
+template bool operator<(const map<unsigned short,
+                                  unsigned short>&,
+                        const map<unsigned short,
+                                  unsigned short>&);
+template bool operator<(const map<int,
+                                  int>&,
+                        const map<int,
+                                  int>&);
+template bool operator<(const map<unsigned int,
+                                  unsigned int>&,
+                        const map<unsigned int,
+                                  unsigned int>&);
+template bool operator<(const map<long,
+                                  long>&,
+                        const map<long,
+                                  long>&);
+template bool operator<(const map<unsigned long,
+                                  unsigned long>&,
+                        const map<unsigned long,
+                                  unsigned long>&);
+template bool operator<(const map<long long,
+                                  long long>&,
+                        const map<long long,
+                                  long long>&);
+template bool operator<(const map<unsigned long long,
+                                  unsigned long long>&,
+                        const map<unsigned long long,
+                                  unsigned long long>&);
+
 
 _namespace_end(std)
