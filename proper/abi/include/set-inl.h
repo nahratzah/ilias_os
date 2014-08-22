a8b8f33bd1753ceaaa15a261a8c223841ebb03d4
Ariane van der Steldt <ariane@stack.nl>
2014-08-22 14:49:14 +0100

    Implement multiset.

--- proper/abi/include/set-inl.h
+++ proper/abi/include/set-inl.h
@@ -493,6 +493,493 @@ void swap(const set<Key, Cmp, A>& x,
 }
 
 
+template<typename Key, typename Cmp, typename A>
+multiset<Key, Cmp, A>::multiset(const Cmp& cmp, const A& a)
+: alloc_base(a),
+  data_(cmp)
+{}
+
+template<typename Key, typename Cmp, typename A>
+template<typename InputIterator>
+multiset<Key, Cmp, A>::multiset(InputIterator b, InputIterator e,
+                      const Cmp& cmp, const A& a)
+: multiset(cmp, a)
+{
+  copy(b, e, inserter(*this, end()));
+}
+
+template<typename Key, typename Cmp, typename A>
+multiset<Key, Cmp, A>::multiset(const multiset& s)
+: multiset(s.begin(), s.end(), s.key_comp(), s.get_allocator_for_copy_())
+{}
+
+template<typename Key, typename Cmp, typename A>
+multiset<Key, Cmp, A>::multiset(multiset&& s)
+: alloc_base(move(s)),
+  data_(move(s.data_))
+{}
+
+template<typename Key, typename Cmp, typename A>
+multiset<Key, Cmp, A>::multiset(const multiset& s, const A& a)
+: multiset(s.begin(), s.end(), s.key_comp(), a)
+{}
+
+template<typename Key, typename Cmp, typename A>
+multiset<Key, Cmp, A>::multiset(multiset&& s, const A& a)
+: multiset(s.key_comp(), a)
+{
+  if (this->get_allocator_() == s.get_allocator_()) {
+    data_ = move(s.data_);
+    size_ = exchange(s.size_, 0);
+  } else {
+    move(s.begin(), s.end(), inserter(*this, end()));
+  }
+}
+
+template<typename Key, typename Cmp, typename A>
+multiset<Key, Cmp, A>::multiset(initializer_list<value_type> il,
+                                const Cmp& cmp, const A& a)
+: multiset(il.begin(), il.end(), cmp, a)
+{}
+
+template<typename Key, typename Cmp, typename A>
+template<typename InputIterator>
+multiset<Key, Cmp, A>::multiset(InputIterator b, InputIterator e, const A& a)
+: multiset(b, e, Cmp(), a)
+{}
+
+template<typename Key, typename Cmp, typename A>
+multiset<Key, Cmp, A>::multiset(initializer_list<value_type> il, const A& a)
+: multiset(il, Cmp(), a)
+{}
+
+template<typename Key, typename Cmp, typename A>
+multiset<Key, Cmp, A>::~multiset() noexcept {
+  clear();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::operator=(const multiset& s) -> multiset& {
+  if (&s != this) {
+    clear();
+    insert(s.begin(), s.end());
+  }
+  return *this;
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::operator=(multiset&& s) -> multiset& {
+  assert(&s != this);
+  clear();
+  if (this->get_allocator_() == s.get_allocator_()) {
+    data_type(move(s.data_)).swap(data_);
+    size_ = exchange(s.size_, 0);
+  } else {
+    move(s.begin(), s.end(), inserter(*this, end()));
+  }
+  return *this;
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::operator=(initializer_list<value_type> il) ->
+    multiset& {
+  clear();
+  insert(il.begin(), il.end());
+  return *this;
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::get_allocator() const noexcept -> allocator_type {
+  return alloc_base::get_allocator();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::begin() noexcept -> iterator {
+  return data_.begin();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::begin() const noexcept -> const_iterator {
+  return data_.begin();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::end() noexcept -> iterator {
+  return data_.end();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::end() const noexcept -> const_iterator {
+  return data_.end();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::rbegin() noexcept -> reverse_iterator {
+  return data_.rbegin();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::rbegin() const noexcept -> const_reverse_iterator {
+  return data_.rbegin();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::rend() noexcept -> reverse_iterator {
+  return data_.rend();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::rend() const noexcept -> const_reverse_iterator {
+  return data_.rend();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::cbegin() const noexcept -> const_iterator {
+  return data_.cbegin();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::cend() const noexcept -> const_iterator {
+  return data_.cend();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::crbegin() const noexcept ->
+    const_reverse_iterator {
+  return data_.crbegin();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::crend() const noexcept -> const_reverse_iterator {
+  return data_.crend();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::empty() const noexcept -> bool {
+  return data_.empty();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::size() const noexcept -> size_type {
+  return size_;
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::max_size() const noexcept -> size_type {
+  return allocator_traits<typename alloc_base::allocator_type>::max_size(
+      this->get_allocator_());
+}
+
+template<typename Key, typename Cmp, typename A>
+template<typename... Args>
+auto multiset<Key, Cmp, A>::emplace(Args&&... args) -> iterator {
+  void* hint = (empty() ?
+                static_cast<void*>(this) :
+                static_cast<void*>(&*data_.root()));
+  auto ptr = impl::new_alloc_deleter<elem>(this->get_allocator_(),
+                                           hint,
+                                           forward<Args>(args)...);
+  auto rv = data_.link(ptr.get(), true);
+  if (rv.second) {
+    ptr.release();
+    ++size_;
+  }
+  return rv.first;
+}
+
+template<typename Key, typename Cmp, typename A>
+template<typename... Args>
+auto multiset<Key, Cmp, A>::emplace_hint(const_iterator pos, Args&&... args) ->
+    iterator {
+  void* hint = (empty() ?
+                static_cast<void*>(this) :
+                static_cast<void*>(&*data_.root()));
+  auto ptr = impl::new_alloc_deleter<elem>(this->get_allocator_(),
+                                           hint,
+                                           forward<Args>(args)...);
+  auto rv = data_.link(pos.impl(), ptr.get(), true);
+  if (rv.second) {
+    ptr.release();
+    ++size_;
+  }
+  return rv.first;
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::insert(const value_type& v) -> iterator {
+  void* hint = (empty() ?
+                static_cast<void*>(this) :
+                static_cast<void*>(&*data_.root()));
+  auto ptr = impl::new_alloc_deleter<elem>(this->get_allocator_(),
+                                           hint,
+                                           v);
+  auto rv = data_.link(ptr.get(), true);
+  if (rv.second) {
+    ptr.release();
+    ++size_;
+  }
+  return rv.first;
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::insert(value_type&& v) -> iterator {
+  void* hint = (empty() ?
+                static_cast<void*>(this) :
+                static_cast<void*>(&*data_.root()));
+  auto ptr = impl::new_alloc_deleter<elem>(this->get_allocator_(),
+                                           hint,
+                                           move(v));
+  auto rv = data_.link(ptr.get(), true);
+  if (rv.second) {
+    ptr.release();
+    ++size_;
+  }
+  return rv.first;
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::insert(const_iterator pos, const value_type& v) ->
+    iterator {
+  void* hint = (empty() ?
+                static_cast<void*>(this) :
+                static_cast<void*>(&*data_.root()));
+  auto ptr = impl::new_alloc_deleter<elem>(this->get_allocator_(),
+                                           hint,
+                                           v);
+  auto rv = data_.link(pos.impl(), ptr.get(), true);
+  if (rv.second) {
+    ptr.release();
+    ++size_;
+  }
+  return rv.first;
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::insert(const_iterator pos, value_type&& v) ->
+    iterator {
+  void* hint = (empty() ?
+                static_cast<void*>(this) :
+                static_cast<void*>(&*data_.root()));
+  auto ptr = impl::new_alloc_deleter<elem>(this->get_allocator_(),
+                                           hint,
+                                           move(v));
+  auto rv = data_.link(pos.impl(), ptr.get(), true);
+  if (rv.second) {
+    ptr.release();
+    ++size_;
+  }
+  return rv.first;
+}
+
+template<typename Key, typename Cmp, typename A>
+template<typename InputIterator>
+auto multiset<Key, Cmp, A>::insert(InputIterator b, InputIterator e) -> void {
+  copy(b, e, inserter(*this, end()));
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::insert(initializer_list<value_type> il) -> void {
+  insert(il.begin(), il.end());
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::erase(const_iterator i) noexcept -> iterator {
+  assert(i != end());
+  iterator rv = data_type::nonconst_iterator(next(i.impl()));
+  auto ptr = impl::existing_alloc_deleter(this->get_allocator_(),
+                                          data_.unlink(i.impl()));
+  if (ptr) --size_;
+  return rv;
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::erase(const key_type& k) noexcept -> size_type {
+  const_iterator i = find(k);
+  if (i == end()) return 0;
+  erase(i);
+  return 1;
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::erase(const_iterator b, const_iterator e)
+    noexcept -> iterator {
+  while (b != e) b = erase(b);
+  return data_type::nonconst_iterator(e.impl());
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::swap(multiset& s) noexcept -> void {
+  using _namespace(std)::swap;
+  data_.swap(s.data_);
+  swap(size_, s.size_);
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::clear() noexcept -> void {
+  data_.unlink_all(impl::alloc_deleter_visitor(this->get_allocator_()));
+  size_ = 0;
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::key_comp() const -> key_compare {
+  return data_.key_comp().get_compare();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::value_comp() const -> key_compare {
+  return data_.value_comp().get_compare();
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::find(const key_type& k) -> iterator {
+  return data_.find(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::find(const key_type& k) const -> const_iterator {
+  return data_.find(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+template<typename K>
+auto multiset<Key, Cmp, A>::find(const K& k) -> iterator {
+  return data_.find(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+template<typename K>
+auto multiset<Key, Cmp, A>::find(const K& k) const -> const_iterator {
+  return data_.find(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::count(const key_type& k) const -> size_type {
+  auto r = data_.equal_range(k);
+  return distance(r.first, r.second);
+}
+
+template<typename Key, typename Cmp, typename A>
+template<typename K>
+auto multiset<Key, Cmp, A>::count(const K& k) const -> size_type {
+  auto r = data_.equal_range(k);
+  return distance(k.first, k.second);
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::lower_bound(const key_type& k) -> iterator {
+  return data_.lower_bound(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::lower_bound(const key_type& k) const ->
+    const_iterator {
+  return data_.lower_bound(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+template<typename K>
+auto multiset<Key, Cmp, A>::lower_bound(const K& k) -> iterator {
+  return data_.lower_bound(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+template<typename K>
+auto multiset<Key, Cmp, A>::lower_bound(const K& k) const -> const_iterator {
+  return data_.lower_bound(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::upper_bound(const key_type& k) -> iterator {
+  return data_.upper_bound(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::upper_bound(const key_type& k) const ->
+    const_iterator {
+  return data_.upper_bound(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+template<typename K>
+auto multiset<Key, Cmp, A>::upper_bound(const K& k) -> iterator {
+  return data_.upper_bound(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+template<typename K>
+auto multiset<Key, Cmp, A>::upper_bound(const K& k) const -> const_iterator {
+  return data_.upper_bound(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::equal_range(const key_type& k) ->
+    pair<iterator, iterator> {
+  return data_.equal_range(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+auto multiset<Key, Cmp, A>::equal_range(const key_type& k) const ->
+    pair<const_iterator, const_iterator> {
+  return data_.equal_range(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+template<typename K>
+auto multiset<Key, Cmp, A>::equal_range(const K& k) ->
+    pair<iterator, iterator> {
+  return data_.equal_range(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+template<typename K>
+auto multiset<Key, Cmp, A>::equal_range(const K& k) const ->
+    pair<const_iterator, const_iterator> {
+  return data_.equal_range(k);
+}
+
+template<typename Key, typename Cmp, typename A>
+bool operator==(const multiset<Key, Cmp, A>& x,
+                const multiset<Key, Cmp, A>& y) {
+  return x.size() == y.size() && equal(x.begin(), x.end(), y.begin(), y.end());
+}
+
+template<typename Key, typename Cmp, typename A>
+bool operator<(const multiset<Key, Cmp, A>& x,
+               const multiset<Key, Cmp, A>& y) {
+  return lexicographical_compare(x.begin(), x.end(), y.begin(), y.end());
+}
+
+template<typename Key, typename Cmp, typename A>
+bool operator!=(const multiset<Key, Cmp, A>& x,
+                const multiset<Key, Cmp, A>& y) {
+  return !(x == y);
+}
+
+template<typename Key, typename Cmp, typename A>
+bool operator>(const multiset<Key, Cmp, A>& x,
+               const multiset<Key, Cmp, A>& y) {
+  return y < x;
+}
+
+template<typename Key, typename Cmp, typename A>
+bool operator<=(const multiset<Key, Cmp, A>& x,
+                const multiset<Key, Cmp, A>& y) {
+  return !(y < x);
+}
+
+template<typename Key, typename Cmp, typename A>
+bool operator>=(const multiset<Key, Cmp, A>& x,
+                const multiset<Key, Cmp, A>& y) {
+  return !(x < y);
+}
+
+template<typename Key, typename Cmp, typename A>
+void swap(const multiset<Key, Cmp, A>& x,
+          const multiset<Key, Cmp, A>& y) noexcept {
+  x.swap(y);
+}
+
+
 _namespace_end(std)
 
 #endif /* _SET_INL_H_ */
--- proper/abi/src/set.cc
+++ proper/abi/src/set.cc
@@ -15,5 +15,110 @@ template class set<unsigned long>;
 template class set<long long>;
 template class set<unsigned long long>;
 
+template bool operator==(const set<char>&,
+                         const set<char>&);
+template bool operator==(const set<signed char>&,
+                         const set<signed char>&);
+template bool operator==(const set<unsigned char>&,
+                         const set<unsigned char>&);
+template bool operator==(const set<short>&,
+                         const set<short>&);
+template bool operator==(const set<unsigned short>&,
+                         const set<unsigned short>&);
+template bool operator==(const set<int>&,
+                         const set<int>&);
+template bool operator==(const set<unsigned int>&,
+                         const set<unsigned int>&);
+template bool operator==(const set<long>&,
+                         const set<long>&);
+template bool operator==(const set<unsigned long>&,
+                         const set<unsigned long>&);
+template bool operator==(const set<long long>&,
+                         const set<long long>&);
+template bool operator==(const set<unsigned long long>&,
+                         const set<unsigned long long>&);
+
+template bool operator<(const set<char>&,
+                        const set<char>&);
+template bool operator<(const set<signed char>&,
+                        const set<signed char>&);
+template bool operator<(const set<unsigned char>&,
+                        const set<unsigned char>&);
+template bool operator<(const set<short>&,
+                        const set<short>&);
+template bool operator<(const set<unsigned short>&,
+                        const set<unsigned short>&);
+template bool operator<(const set<int>&,
+                        const set<int>&);
+template bool operator<(const set<unsigned int>&,
+                        const set<unsigned int>&);
+template bool operator<(const set<long>&,
+                        const set<long>&);
+template bool operator<(const set<unsigned long>&,
+                        const set<unsigned long>&);
+template bool operator<(const set<long long>&,
+                        const set<long long>&);
+template bool operator<(const set<unsigned long long>&,
+                        const set<unsigned long long>&);
+
+
+template class multiset<char>;
+template class multiset<signed char>;
+template class multiset<unsigned char>;
+template class multiset<short>;
+template class multiset<unsigned short>;
+template class multiset<int>;
+template class multiset<unsigned int>;
+template class multiset<long>;
+template class multiset<unsigned long>;
+template class multiset<long long>;
+template class multiset<unsigned long long>;
+
+template bool operator==(const multiset<char>&,
+                         const multiset<char>&);
+template bool operator==(const multiset<signed char>&,
+                         const multiset<signed char>&);
+template bool operator==(const multiset<unsigned char>&,
+                         const multiset<unsigned char>&);
+template bool operator==(const multiset<short>&,
+                         const multiset<short>&);
+template bool operator==(const multiset<unsigned short>&,
+                         const multiset<unsigned short>&);
+template bool operator==(const multiset<int>&,
+                         const multiset<int>&);
+template bool operator==(const multiset<unsigned int>&,
+                         const multiset<unsigned int>&);
+template bool operator==(const multiset<long>&,
+                         const multiset<long>&);
+template bool operator==(const multiset<unsigned long>&,
+                         const multiset<unsigned long>&);
+template bool operator==(const multiset<long long>&,
+                         const multiset<long long>&);
+template bool operator==(const multiset<unsigned long long>&,
+                         const multiset<unsigned long long>&);
+
+template bool operator<(const multiset<char>&,
+                        const multiset<char>&);
+template bool operator<(const multiset<signed char>&,
+                        const multiset<signed char>&);
+template bool operator<(const multiset<unsigned char>&,
+                        const multiset<unsigned char>&);
+template bool operator<(const multiset<short>&,
+                        const multiset<short>&);
+template bool operator<(const multiset<unsigned short>&,
+                        const multiset<unsigned short>&);
+template bool operator<(const multiset<int>&,
+                        const multiset<int>&);
+template bool operator<(const multiset<unsigned int>&,
+                        const multiset<unsigned int>&);
+template bool operator<(const multiset<long>&,
+                        const multiset<long>&);
+template bool operator<(const multiset<unsigned long>&,
+                        const multiset<unsigned long>&);
+template bool operator<(const multiset<long long>&,
+                        const multiset<long long>&);
+template bool operator<(const multiset<unsigned long long>&,
+                        const multiset<unsigned long long>&);
+
 
 _namespace_end(std)
