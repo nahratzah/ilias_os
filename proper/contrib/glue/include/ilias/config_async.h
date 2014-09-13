7821c1e27a1af6b29daf25fccf0cc22b32eb759a
Ariane van der Steldt <ariane@stack.nl>
2014-09-13 23:36:58 +0100

    Import workq and corresponding promise callback.

--- proper/contrib/glue/include/ilias/config_async.h
+++ proper/contrib/glue/include/ilias/config_async.h
@@ -4,4 +4,7 @@
 /* Check if shared_ptr has atomic operation support. */
 #define ILIAS_ASYNC_HAS_ATOMIC_SHARED_PTR 1
 
+/* Declare that we don't want blocking code. */
+#define ILIAS_ASYNC_NO_SYNC_CODE 1
+
 #endif /* ILIAS_ASYNC_CONFIG_H */
