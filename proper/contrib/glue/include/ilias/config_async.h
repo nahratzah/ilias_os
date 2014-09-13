6cfc1bfad4ec9fde9bb8a0a6162e7a223295e6ae
Ariane van der Steldt <ariane@stack.nl>
2014-09-13 17:59:18 +0100

    Create auto-generated config for ilias_async in kernel mode.

---
+++ proper/contrib/glue/include/ilias/config_async.h
@@ -0,0 +1,7 @@
+#ifndef ILIAS_ASYNC_CONFIG_H
+#define ILIAS_ASYNC_CONFIG_H
+
+/* Check if shared_ptr has atomic operation support. */
+#define ILIAS_ASYNC_HAS_ATOMIC_SHARED_PTR 1
+
+#endif /* ILIAS_ASYNC_CONFIG_H */
